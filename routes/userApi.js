a3eee72cb2cd7d3604500b68180f00fa662840d5
Eshwar0601 <852eshwar@gmail.com>
2021-07-27 09:05:26 +0530

    integrated with express validator

--- routes/userApi.js
+++ routes/userApi.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const {check, validationResult} = require("express-validator");
 
 router.get("/", (req, res) => {
+    // console.log(req.body);
     res.send("Users route.");
 });
+
+
+router.post("/",
+    [
+        check("name", "Name is required").not().isEmpty(),
+        check("email", "please enter a valid email").isEmail(), 
+        check("password", "please password should have at least 5 characters").isLength({min: 5})
+    ], 
+    (req, res) => {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({errors : errors.array()})
+        }
+        res.send("Users route.");
+});
+
 
 module.exports = router;
