46ba06b9a9fe8d8f41bb87d892f4acf785cb3668
Eshwar0601 <852eshwar@gmail.com>
2021-07-27 09:26:39 +0530

    save user data on mongo DB

--- routes/userApi.js
+++ routes/userApi.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {check, validationResult} = require("express-validator");
+const User = require("../models/User");
 
 router.get("/", (req, res) => {
     // console.log(req.body);
@@ -14,13 +15,27 @@ router.post("/",
         check("email", "please enter a valid email").isEmail(), 
         check("password", "please password should have at least 5 characters").isLength({min: 5})
     ], 
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
 
         if(!errors.isEmpty()) {
             return res.status(400).json({errors : errors.array()})
         }
-        res.send("Users route.");
+        try {
+            const {name, email, password} = req.body;
+            let user = await User.findOne({email : email})
+            if(user) {
+                return res.status(400).json({errors: [{msg: "User already exists"}]});
+            }
+            user = new User({
+                name, email, password
+            });
+            // user.save();
+            res.send("Users created.");
+        } catch (error) {
+            console.log(error);
+        }
+        
 });
 
 
